535dd410c4e7e16becaf1ef53f2e243e69865a20
Ilyagavrilin <89260891+Ilyagavrilin@users.noreply.github.com>
2021-08-21 07:55:32 +0300

    Update kvadratnoe.c

--- kvadratnoe.c
+++ kvadratnoe.c
@@ -2,33 +2,39 @@
 #include <math.h>
 #include <locale.h>
 
+float linear_solution(float a, float b){ /* ax + b = 0*/
+    float x = -b/a;
+    return x;
+}
+void disc_solution(float D, float a, float b){
+    float x1  = (-b + sqrt(D))/(2*a);
+    float x2  = (-b - sqrt(D))/(2*a);
+    printf("x1 = %f x2 = %f\n", x1, x2);
+}
+
 int main(){
     setlocale(LC_ALL, "Rus");
     float a;
     float b;
     float c;
-    printf("Решение квадартного уравнения вида ax^2+bx+c=0\n");
-    printf("Введите коэффициент a: ");
+    printf("Решение квадратного уравненения вида ax^2+bx+c=0\n");
+    printf("введите коэффициент a: ");
     scanf("%f", &a);
-    printf("Введите коэффициент b: ");
+    printf("введите коэффициент b: ");
     scanf("%f", &b);
-    printf("Введите коэффициент c: ");
+    printf("введите коэффициент c: ");
     scanf("%f", &c);
     if ((int) a == 0){
-        if ((int) b == 0){printf("x - любое число.");}
-        else{printf("x = %f\n", -c/b);}
+        if ((int) b == 0){printf("x - любое число");}
+        else{printf("x = %f\n", linear_solution(b, c));}
     }
     else{
         float D = pow(b, 2) - 4 * a * c;
         if (D>= 0){
-            if (D > 0){
-                float x1  = (-b + sqrt(D))/(2*a);
-                float x2  = (-b - sqrt(D))/(2*a);
-                printf("x1 = %f x2 = %f\n", x1, x2);
-            }
+            if (D > 0){disc_solution(D, a, b);}
             else{printf("x = %f\n", -b/(2*a));}
         }
-        else{printf("Нет действительных корней.\n");}
+        else{printf("Нет действительных корней\n");}
     }
     return 0;
 }

e4a30c9f435e399ff11609076e1a19ee3495a960
Ilyagavrilin <89260891+Ilyagavrilin@users.noreply.github.com>
2021-08-22 16:50:37 +0300

    Update kvadratka_new.c

--- kvadratka_new.c
+++ kvadratka_new.c
@@ -26,77 +26,80 @@ int Movement();
 int Tests();
 
 
-int main(){
-    if (!Tests()){
+int main() {
+    if (!Tests()) {
             printf("Error.\n");
             return 0;}
-    else{
+    else {
         printf("Tests successfully passed.\n");
     }
-    if (Movement() != -1){return 0;}
-    else{
+    if (Movement() != -1) return 0;
+    else {
         printf("Error.\n");
         return 0;
     }
 }
 
-int input(double *factor, char name){
-        if (factor == NULL){
+int input(double *factor, char name) {
+        if (factor == NULL) {
+            printf("No declared variable factor.\n");
             return FALSE;
         }
         printf("Enter coefficient %c:", name);
-        if (scanf("%lf", factor) == 1){
+        if (scanf("%lf", factor) == 1) {
             return TRUE;
         }
-        else{
+        else {
+            printf("Wrong format in input data.\n");
             return FALSE;
         }
 }
 
 
-int is_zero(double num){
+int is_zero(double num) {
         if (num > -(10e-8) && num < 10e-8){
             return TRUE;
         }
-        else{
+        else {
             return FALSE;
         }
 }
 
 
-int Linear_solution(double a, double b, double *x){
+int Linear_solution(double a, double b, double *x) {
     if (x == NULL){
+        printf("Error in solving linear equation.\n");
         return -1;
     }
-    if (is_zero(a) && is_zero(b)){
+    if (is_zero(a) && is_zero(b)) {
         return 8;
     }
-    else if (is_zero(a) && !is_zero(b)){
+    else if (is_zero(a) && !is_zero(b)) {
         return 0;
     }
-    else{
+    else {
         *x = -b/a;
         return 1;
     }
 }
 
 
-int Kvadratic_solution(double a, double b, double c, double *x1, double *x2){
-    if ((x1 == NULL) || (x2 == NULL)){
+int Kvadratic_solution(double a, double b, double c, double *x1, double *x2) {
+    if ((x1 == NULL) || (x2 == NULL) || (x1 == x2)) {
+        printf("Error in input variable.\n");
         return -1;
     }
 
-    if (is_zero(a)){
-        int n = Linear_solution(b, c, x1);
-        return n;
+    if (is_zero(a)) {
+        return Linear_solution(b, c, x1);
     }
 
-    else if (!is_zero(a) && is_zero(b) && is_zero(c)){
+    else if (is_zero(b) && is_zero(c)) {
         *x1 = 0;
         return 1;
     }
 
-    else if (!is_zero(a) && !is_zero(b) && is_zero(c)){
+    else if (is_zero(c)) {
         *x1 = 0;
         Linear_solution(b, c, x2);
         return 2;
@@ -105,71 +108,71 @@ int Kvadratic_solution(double a, double b, double c, double *x1, double *x2){
     else {/* contains 111 or 101*/
         double D = pow(b, 2) - 4 *a * c;
 
-        if (D > 0){
-            *x1 = (-b + sqrt(D))/(2*a);
-            *x2 = (-b - sqrt(D))/(2*a);
+        if (D > 0) {
+            *x1 = (-b + sqrt(D)) / (2 * a);
+            *x2 = (-b - sqrt(D)) / (2 * a);
 
             return 2;
         }
-        else if (is_zero(D)){
+        else if (is_zero(D)) {
             *x1 = -b/(2*a);
-            return 1; 
+            return 1;
         }
 
-        else{
+        else {
             return 0;
         }
     }
 }
 
 
-int Movement(){
+int Movement() {
     double a = 0;
     double b = 0;
     double c = 0;
- 
+
     double x1 = 0;
     double x2 = 0;
- 
+
     char A = 'a';
     char B = 'b';
     char C = 'c';
 
     printf("This program can solve kvadratic equations ax^2+bx+c=0.\n");
-    if (!input(&a, A) || !input(&b, B) || !input(&c, C)){
+    if (!input(&a, A) || !input(&b, B) || !input(&c, C)) {
       return -1;
     }
-        
+
     int res = 0;
     res = Kvadratic_solution(a, b, c, &x1, &x2);
-    
+
     if (res == 8){printf("X - each rational number.\n");}
-    else if (res == 0){printf("No rational roots.\n");}
-    else if (res == 1){printf("x = %f\n", x1);}
-    else if (res == 2){printf("x1 = %f, x2 = %f\n", x1, x2);}
+    else if (res == 0) printf("No rational roots.\n");
+    else if (res == 1) printf("x = %.3lf\n", x1);
+    else if (res == 2) printf("x1 = %.3lf, x2 = %.3lf\n", x1, x2);
     else{return -1;}
-        
+
     return 0;
 }
- 
- 
+
+
  int Tests(){
-    int a[5] = {0, 1, 0, 0, 1};
-    int b[5] = {0,0, 2, 0, 4};
-    int c[5] = {0,0, -6, 1, 3};
-    int r_x1[5] = {0, 0, 3, 0, -1};
-    int r_x2[5] = {0, 0, 0, 0, -3};
-    int res[5] = {8, 1, 1, 0, 2};
+    double a[5] =    {0, 1,  0, 0,  1};
+    double b[5] =    {0, 0,  2, 0,  4};
+    double c[5] =    {0, 0, -6, 1,  3};
+    double r_x1[5] = {0, 0,  3, 0, -1};
+    double r_x2[5] = {0, 0,  0, 0, -3};
+    int res[5] =     {8, 1,  1, 0,  2};
 
     int result = 0;
 
-    for (int i = 0; i < 5; i++){
+    for (int i = 0; i < 5; i++) {
         double x1 = 0;
         double x2 = 0;
 
         result = Kvadratic_solution(a[i], b[i], c[i], &x1, &x2);
 
-        if ((int)x1 != r_x1[i] || (int) x2 != r_x2[i] || result != res[i]){
+        if (!is_zero(x1-r_x1[i]) || !is_zero(x2-r_x2[i]) || result != res[i]) {
             printf("Code failed on test %d.\n", i + 1);
             return 0;
         }

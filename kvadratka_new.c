5ca03ba1e396f931f5edb4bb15cfc2ee5e45a19e
Ilyagavrilin <89260891+Ilyagavrilin@users.noreply.github.com>
2021-08-26 18:18:52 +0300

    Update kvadratka_new.c

--- kvadratka_new.c
+++ kvadratka_new.c
@@ -1,3 +1,8 @@
+/*! \file
+* \brief This program solve square equations ax^2 + bx + c = 0
+* Program can works with non complex coefficients, has self test unit. Can accept equations while user don't type n(no)
+*/
+
 #include <stdio.h>
 #include <math.h>
 
@@ -5,151 +10,250 @@
 #define FALSE 0
 
 
-/* inputs double number, write on display name of variable, returns true/false */
+const int ERROR = -1;
+const int INFINITIVE = 8;
+const int ZERO_ANS = 0;
+const int ONE_ANS = 1;
+const int TWO_ANS = 2;
+
+/*! \brief inputs double number, write on display name of variable, returns true if scan is ok, other case false
+*   \param [in] name Accepts name of variable, and writes it on display
+*   \param [out] factor Input double writes to this variable
+*   \return 1 if param wrote into factor, else 0
+*/
 int input(double *factor, char name);
 
-/* if number is zero returns true, other returns false */
-int is_zero(double num);
+/*! \brief function tests if double number is zero
+*   \param [in] val accepts number
+*   \return 1 if absolute val is lover than eps, 0 in other cases
+ */
+int is_zero(double val);
 
-/*solution of linear equation(ax + b = 0), write answer in variable x,
-can operate with a == 0 and/or b == 0, returns number of roots (8 means infinity),  error code is -1*/
+int dbl_equals(double v1, double v2) {
+    return fabs(v1 - v1) < DBL_EPSILON;
+}
+
+/*! \brief solution of linear equation(ax + b = 0)
+*   \param [in] a, b coefficients of input linear equations
+*   \param [out] x root of linear equation
+*   \return number of roots, or Error
+*/
 int Linear_solution(double a, double b, double *x);
 
-/*solution of kvadratic equation(ax^2 + bx + c = 0), write answer in variable x1 and x2,
- if answer is the only one writes it in x1, x2 don't changes,
-can operate with a == 0 and/or b == 0 and/or c == 0, returns number of roots(0, 1 ,2, 8) (8 means infinity), error code is -1*/
-int Kvadratic_solution(double a, double b, double c, double *x1, double *x2);
+/*! \brief solution of square equation(ax^2 + bx + c = 0)
+*   \param [in] a, b, c coefficients of input square equation
+*   \param [out] x1, x2 roots of square equation
+*   \return number of roots, or Error
+*   \warning can't works with complex coefficients, don't solve equations with complex roots
+*/
+int Square_solution(double a, double b, double c, double *x1, double *x2);
 
-/* runs main program, returns false in cause of error*/
-int Movement();
+/*! \brief runs main program
+*   \return false in cause of error, else 0*/
+int Solver();
 
-/* works only for 6 digits, idk why*/
+/*! \brief cuts number after dot in double number
+*   \param [in] num value witch need to cut numbers
+*   \param [in] precision value of signs after dot in input number
+*   \return number with cut signs after dot
+*/
 double num_cutter(double num, int precision);
 
+/*! \brief cheks if input number is NAN
+*   \param [in] x number needed to check
+*   \return TRUE if x is NAN, else FALSE
+*/
+int is_nan(double x);
+
+/*! \brief makes letter to lower
+*   \param [in] letter input letter
+*   \return input letter, but lower
+*/
+int lower(char letter);
+
+/*! \brief ask user for solving new equation
+*   \return Error if function doesn't finished right
+*/
+int ProgramContinuer();
+
+/*! \brief unit tests
+*   \return Error if tests don't finished, and writes number of failed test
+*/
 int Tests();
+
+int testRunner();
+
 
 
 int main() {
-    if (!Tests()) {
-            printf("Error.\n");
-            return 0;}
-    else {
-        printf("Tests successfully passed.\n");
-    }
-    if (Movement() != -1) return 0;
-    else {
-        printf("Error.\n");
+    if (testRunner() == ERROR) {
+        printf("Error in tests.\n");
         return 0;
     }
+
+
+    printf ("This program can solve square equations ax^2+bx+c=0.\n");
+
+    if (ProgramContinuer() == ERROR) {
+        printf("Error in solving equation.\n");
+        return 0;
+    }
+
+    return 0;
 }
 
-int input(double *factor, char name) {
-        if (factor == NULL) {
-            printf("No declared variable factor.\n");
+
+int input (double *link , char name) {
+        if (link == NULL) {
             return FALSE;
         }
-        printf("Enter coefficient %c:", name);
-        if (scanf("%lf", factor) == 1) {
+
+        printf("Enter coefficient %c: ", name);
+
+        if (scanf("%lf", link) == 1) {
             return TRUE;
-        }
-        else {
-            printf("Wrong format in input data.\n");
+        } else {
             return FALSE;
         }
 }
 
-
-int is_zero(double num) {
-        if (num > -(10e-8) && num < 10e-8){
-            return TRUE;
-        }
-        else {
-            return FALSE;
-        }
+int is_zero (double val) {
+    const double precision = 10e-8;///<precision in test
+    return (val > -precision && val < precision);
 }
 
 
-int Linear_solution(double a, double b, double *x) {
+int lower(char letter) {
+    if (letter >= 65 && letter <= 90) {
+        return letter + 32;
+    }
+    else if (letter >= 97 && letter <= 122) {
+        return letter;
+    }
+    else {
+        return ERROR;
+    }
+}
+
+// ax + b = 0
+int Linear_solution (double a, double b, double *x) {
     if (x == NULL){
-        printf("Error in solving linear equation.\n");
-        return -1;
+        return ERROR;
     }
     if (is_zero(a) && is_zero(b)) {
-        return 8;
+        return INFINITIVE;
     }
     else if (is_zero(a) && !is_zero(b)) {
-        return 0;
+        return ZERO_ANS;
     }
     else {
         *x = -b/a;
-        return 1;
+        return ONE_ANS;
     }
 }
 
-
-int Kvadratic_solution(double a, double b, double c, double *x1, double *x2) {
+// ax^2 + bx + c = 0
+int Square_solution (double a, double b, double c, double *x1, double *x2) {
     if ((x1 == NULL) || (x2 == NULL) || (x1 == x2)) {
-        printf("Error in input variable.\n");
-        return -1;
+        return ERROR;
     }
 
     if (is_zero(a)) {
-        if (is_zero(b)) {
-            if (is_zero(c)) return 8;
+        // bx + c = 0;
 
-            else return 0;
+        if (is_zero(b)) {
+            if (is_zero(c)) return INFINITIVE;
+
+            else return ZERO_ANS;
         }
         else {
             return Linear_solution(b, c, x1);
         }
     }
     else {
-        double D = pow(b, 2) - 4 *a * c;
+        // ax^2 + bx + c = 0
+        const double D = b * b - 4 * a * c;
 
         if (D > 0) {
-            *x1 = (-b + sqrt(D)) / (2 * a);
-            *x2 = (-b - sqrt(D)) / (2 * a);
+            const double sq_D = sqrt (D);
 
-            return 2;
-        }
-        else if (is_zero(D)) {
-            *x1 = -b/(2*a);
-            return 1;
-        }
+            *x1 = (-b + sq_D) / (2 * a);
+            *x2 = (-b - sq_D) / (2 * a);
 
+            return TWO_ANS;
+        }
+        else if (is_zero (D)) {
+            *x1 = -b / (2 * a);
+            return ONE_ANS;
+        }
         else {
-            return 0;
+            return ZERO_ANS;
         }
     }
 
 }
 
+int ProgramContinuer() {
+    const char answer_yes = 'y';
+    const char answer_no = 'n';
+    char answer = 0;
 
-int Movement() {
+    do {
+        if (Solver() == ERROR) {
+                return ERROR;
+        }
+
+        printf("Continue program? Y/N \n");
+        scanf("%s", &answer);
+
+        if (answer == answer_no) {
+            printf("Program finished.\n");
+            break;
+        }
+        else if (answer == answer_yes) {
+            continue;
+        }
+        else {
+            printf("Wrong answer, program finished.\n");
+            break;
+        }
+    } while (lower(answer) == answer_yes);
+    return 0;
+}
+
+int Solver() {
     double a = 0;
     double b = 0;
     double c = 0;
 
-    double x1 = 0;
-    double x2 = 0;
+    double* form[3] = {&a, &b, &c};
+    char name[3] = {'a', 'b', 'c'};
 
-    char A = 'a';
-    char B = 'b';
-    char C = 'c';
+    for (int i = 0; i < 3; i++) {
+        if (input(form[i], name[i]) == FALSE) {
+            return ERROR;
+        }
+        }
 
-    printf("This program can solve kvadratic equations ax^2+bx+c=0.\n");
-    if (!input(&a, A) || !input(&b, B) || !input(&c, C)) {
-      return -1;
+    double x1 = NAN;
+    double x2 = NAN;
+    int numberRoots = 0;
+
+    numberRoots = Square_solution (a, b, c, &x1, &x2);
+
+    switch (numberRoots) {
+        case ZERO_ANS: printf ("No rational roots.\n");
+                       break;
+
+        case ONE_ANS:  printf("x = %.3lf\n", x1);
+                       break;
+
+        case TWO_ANS:  printf ("x1 = %.3lf, x2 = %.3lf\n", x1, x2);
+                       break;
+
+        default:       return ERROR;
+                       break;
     }
-
-    int res = 0;
-    res = Kvadratic_solution(a, b, c, &x1, &x2);
-
-    if (res == 8){printf("X - each rational number.\n");}
-    else if (res == 0) printf("No rational roots.\n");
-    else if (res == 1) printf("x = %.3lf\n", x1);
-    else if (res == 2) printf("x1 = %.3lf, x2 = %.3lf\n", x1, x2);
-    else{return -1;}
 
     return 0;
 }
@@ -161,26 +265,61 @@ double num_cutter(double num, int precision) {
 }
 
 
- int Tests(){
-    double a[7] =    {0, 1,  0, 0,  1, -1,  0.1};
-    double b[7] =    {0, 0,  2, 0,  4,  0,  4};
-    double c[7] =    {0, 0, -6, 1,  3,  4,  0.6};
-    double r_x1[7] = {0, 0,  3, 0, -1, -2, -0.151};
+int is_nan(double x) {
+    return x != x;
+}
+
+ int Tests() {
+    double a[7] =    {0, 1,  0, 0,  1, -1,  0.1   };
+    double b[7] =    {0, 0,  2, 0,  4,  0,  4     };
+    double c[7] =    {0, 0, -6, 1,  3,  4,  0.6   };
+    double r_x1[7] = {0, 0,  3, 0, -1, -2, -0.151 };
     double r_x2[7] = {0, 0,  0, 0, -3,  2, -39.850};
-    int res[7] =     {8, 1,  1, 0,  2,  2,  2};
+    int res[7] =     {8, 1,  1, 0,  2,  2,  2     };
 
     int result = 0;
 
     for (int i = 0; i < 7; i++) {
-        double x1 = 0;
-        double x2 = 0;
+        double x1 = NAN;
+        double x2 = NAN;
 
-        result = Kvadratic_solution(a[i], b[i], c[i], &x1, &x2);
+        result = Square_solution(a[i], b[i], c[i], &x1, &x2);
 
-        if (!is_zero(num_cutter(x1, 3)-r_x1[i]) || !is_zero(num_cutter(x2, 3)-r_x2[i]) || result != res[i]) {
+        if ((res[i] == 0 || res[i] == 8) && (!is_nan(x1) || !is_nan(x2))) {
+            printf("Code failed on test %d.\n", i + 1);
+            return 0;
+        }
+        else if (res[i] == 1 && (!is_zero(num_cutter(x1, 3)-r_x1[i]) || !is_nan(x2))) {
+            printf("Code failed on test %d.\n", i + 1);
+            return 0;
+        }
+        else if (res[i] == 2 &&
+        (!is_zero(num_cutter(x1, 3)-r_x1[i]) || !is_zero(num_cutter(x2, 3)-r_x2[i]) || result != res[i])) {
             printf("Code failed on test %d.\n", i + 1);
             return 0;
         }
     }
     return 1;
+}
+
+int testRunner() {
+    const char answer_yes = 'y';
+    const char answer_no = 'n';
+    char answer = 0;
+
+    printf("Run tests?(Y/N) \n");
+    scanf("%s", &answer);
+    if (lower(answer) == answer_yes) {
+        if (!Tests()) {
+            printf("Error in tests.\n");
+            return ERROR;
+        } else {
+            printf("Tests successfully passed.\n");
+        }
+    }
+    else if (lower(answer) != answer_no) {
+        printf("Error in answer.\n");
+        return ERROR;
+    }
+    return 0;
 }
